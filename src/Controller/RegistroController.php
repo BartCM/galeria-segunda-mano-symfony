aa21ad4c6505200376c41bb77ae8cf4ee1286b03
BARTOLOME CANTO MESAS <bartolocan@hotmail.com>
2026-01-17 13:48:36 +0100

    Creacion Login y registro

---
+++ src/Controller/RegistroController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Usuario;
+use App\Form\RegistroType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistroController extends AbstractController
+{
+    #[Route('/registro', name: 'app_registro')]
+    public function registro(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $usuario = new Usuario();
+        $form = $this->createForm(RegistroType::class, $usuario);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $usuario->setPassword(
+                $passwordHasher->hashPassword(
+                    $usuario,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($usuario);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registro/registro.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
